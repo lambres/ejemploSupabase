b1d9cb69e11a3785ca12f43277b782cf3948a182
lambres <lambres@gmail.com>
2025-04-13 23:18:48 -0300

    inicio

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,6 +1,6 @@
 export const environment = {
     production: false,
-    apiUrl: 'api-url',
-    publicAnonKey: 'public-anon-key'
+    apiUrl: 'https://shruhwqmotwnqxmxufje.supabase.co',
+    publicAnonKey: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InNocnVod3Ftb3R3bnF4bXh1ZmplIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDQ1OTUxNjIsImV4cCI6MjA2MDE3MTE2Mn0.jF0QYfu1SYzsr1CoUeodwAMnHtN0mMCdQda4xzSw5jA'
   };
 
